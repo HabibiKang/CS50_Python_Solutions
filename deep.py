b617d4d0159bce9039efe3db4f069dcfd6314e81
Adam Abbas <125621684+HabibiKang@users.noreply.github.com>
2023-12-08 18:33:46 -0500

    Create deep.py

---
+++ deep.py
@@ -0,0 +1,13 @@
+answer = input("What is the answer to the Great Question of life, the Universe and everything?\n").strip()
+
+if answer == "42":
+    print("Yes")
+
+elif answer == "forty-two":
+    print("Yes")
+
+elif answer == "Forty Two":
+    print("Yes")
+
+else:
+    print("No")
